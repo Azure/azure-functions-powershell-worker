71c33178b9c218b24461d5f59fe761d3a9c01f15
Tyler Leonhardt <tyler@Tylers-MacBook-Pro.local>
2018-08-31 16:27:46 -0700

    add a comment to explain why execution policy is needed

--- src/PowerShell/PowerShellManager.cs
+++ src/PowerShell/PowerShellManager.cs
@@ -28,8 +28,12 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.PowerShell
         internal PowerShellManager(RpcLogger logger)
         {
             var initialSessionState = InitialSessionState.CreateDefault();
+            
+            // Setting the execution policy on macOS and Linux throws an exception so only update it on Windows
             if(Platform.IsWindows)
             {
+                // This sets the execution policy on Windows to Unrestricted which is required to run the user's function scripts on
+                // Windows client versions. This is needed if a user is testing their function locally with the func CLI
                 initialSessionState.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
             }
             _pwsh = PowerShell.Create(initialSessionState);
