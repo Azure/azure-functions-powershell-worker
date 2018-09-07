14f99099b5bd5b7d822dec0ce972d1d03fb1b7e8
Dongbo Wang <dongbow@microsoft.com>
2018-09-07 11:22:24 -0700

    Add comments to the example function

--- examples/PSCoreApp/MyHttpTrigger/run.ps1
+++ examples/PSCoreApp/MyHttpTrigger/run.ps1
@@ -1,11 +1,22 @@
+# Trigger the function by running Invoke-RestMethod:
+#    (via get method): Invoke-RestMethod -Uri http://localhost:7071/api/MyHttpTrigger?Name=Joe
+#   (via post method): Invoke-RestMethod `
+#                        -Uri http://localhost:7071/api/MyHttpTrigger `
+#                        -Method Post `
+#                        -Body (ConvertTo-Json @{ Name="Joe" }) `
+#                        -Headers @{'Content-Type' = 'application/json' }`
+
+# Input bindings are passed in via param block.
 param($req, $TriggerMetadata)
 
+# You can write to the Azure Functions log streams as you would in a normal PowerShell script.
 Write-Verbose "PowerShell HTTP trigger function processed a request." -Verbose
 
-if($req.Query.Name -or $req.Body.Name) {
-    $name = $req.Query.Name
-    if (-not $name) { $name = $req.Body.Name }
+# You can interact with query parameters, the body of the request, etc.
+$name = $req.Query.Name
+if (-not $name) { $name = $req.Body.Name }
 
+if($name) {
     $status = 200
     $body = "Hello " + $name
 }
@@ -14,6 +25,7 @@ else {
     $body = "Please pass a name on the query string or in the request body."
 }
 
+# You associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
     StatusCode = $status
     Body = $body
