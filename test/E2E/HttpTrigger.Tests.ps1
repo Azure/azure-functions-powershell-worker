5d946f15de50ebb4235e0482462d6fbdc114e150
Tyler Leonhardt <tylerl0706@gmail.com>
2018-10-29 10:47:01 -0700

    add additional test cases

--- test/E2E/HttpTrigger.Tests.ps1
+++ test/E2E/HttpTrigger.Tests.ps1
@@ -9,10 +9,34 @@ Describe 'HttpTrigger Tests' {
         & "$PSScriptRoot/setupE2Etests.ps1"
         { Invoke-RestMethod $FUNCTIONS_BASE_URL } | Should -Not -Throw -Because 'The E2E tests require a Function App to be running on port 7071'
     }
+
     AfterAll {
         Get-Job -Name FuncJob -ErrorAction SilentlyContinue | Stop-Job | Remove-Job
     }
-    It "Test's a basic HttpTrigger function" {
-        Invoke-RestMethod "$FUNCTIONS_BASE_URL/api/TestBasicHttpTrigger?Name=Atlas" | Should -Be 'Hello Atlas'
+
+    It "Test basic HttpTrigger function" -TestCases @(
+        @{ Name = 'Atlas'; StatusCode = 200; Content = 'Hello World' },
+        @{ Name = $null; StatusCode = 400 },
+        @{ StatusCode = 400 }
+    ) -Test {
+        param ($Name, $StatusCode, $Content)
+
+        if (Test-Path 'variable:Name') {
+            $url = "$FUNCTIONS_BASE_URL/api/TestBasicHttpTrigger?Name=$Name"
+        } else {
+            $url = "$FUNCTIONS_BASE_URL/api/TestBasicHttpTrigger"
+        }
+
+
+        try {
+            $res = Invoke-WebRequest $url
+        } catch {
+            $res = $_.Exception.Response
+        }
+        [int]$res.StatusCode | Should -Be $StatusCode
+
+        if ($Content) {
+            $res.Content | Should -Be $Content
+        }
     }
 }
