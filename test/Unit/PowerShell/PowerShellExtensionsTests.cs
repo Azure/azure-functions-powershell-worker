293f6086a763233977fcfd07e89c23930e204a2d
Tyler James Leonhardt <tylerl0706@gmail.com>
2018-11-12 15:00:08 -0800

    Log items added to the pipeline (#95)
    * Log items added to the pipeline
    
    * CRLF and LF
    
    * add comment about Write-Output

--- test/Unit/PowerShell/PowerShellExtensionsTests.cs
+++ test/Unit/PowerShell/PowerShellExtensionsTests.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
-
+using System.Collections;
 using System.Collections.ObjectModel;
 
 using Microsoft.Azure.Functions.PowerShellWorker.PowerShell;
@@ -10,7 +10,9 @@ using Xunit;
 
 namespace Microsoft.Azure.Functions.PowerShellWorker.Test
 {
+    using System;
     using System.Management.Automation;
+    using System.Runtime.InteropServices;
 
     public class PowerShellExtensionsTests
     {
@@ -35,6 +37,25 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Test
             Assert.Empty(pwsh.Commands.Commands);
 
             Assert.Single(results, data);
+        }
+
+        [Fact]
+        public void CanFormatObjectsToString()
+        {
+            string[] expectedResult = {
+                "",
+                "Name                           Value",
+                "----                           -----",
+                "Foo                            bar",
+                "",
+                "",
+                ""
+            };
+
+            Hashtable data = new Hashtable {{ "Foo", "bar"}};   
+            Assert.Equal(
+                string.Join(Environment.NewLine, expectedResult), 
+                PowerShell.Create().FormatObjectToString(data));
         }
     }
 }
