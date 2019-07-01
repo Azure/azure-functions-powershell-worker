681fb5dc3e26e0e21a733662a378d5e2460fa8ba
Anatoli Beliaev <anatolib@microsoft.com>
2019-07-01 16:45:30 -0700

    Output Az path from UsingManagedDependencies

--- test/E2E/TestFunctionApp/UsingManagedDependencies/run.ps1
+++ test/E2E/TestFunctionApp/UsingManagedDependencies/run.ps1
@@ -3,5 +3,5 @@ param($req)
 Write-Verbose "PowerShell HTTP trigger function processed a request." -Verbose
 
 Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
-    Body = Get-Module Az -ListAvailable | fl | Out-String
+    Body = Get-Module Az -ListAvailable | % Path
 })
