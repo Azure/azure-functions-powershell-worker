17fda5fe8e82a0d1b945d79c82f258e958567a3b
Francisco Gomez Gamino <frangom@microsoft.com>
2019-07-24 21:02:07 -0700

    Changing default configuration from 'Debug' to 'Release' for the PowerShell worker source path.

--- test/E2E/Start-E2ETest.ps1
+++ test/E2E/Start-E2ETest.ps1
@@ -39,7 +39,7 @@ Expand-Archive $output -DestinationPath $FUNC_CLI_DIRECTORY
 
 Write-Host "Copying azure-functions-powershell-worker to Functions Host workers directory..."
 
-$configuration = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { 'Debug' }
+$configuration = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { 'Release' }
 Remove-Item -Recurse -Force -Path "$FUNC_CLI_DIRECTORY/workers/powershell"
 Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/netcoreapp2.2/publish/" "$FUNC_CLI_DIRECTORY/workers/powershell"
 
