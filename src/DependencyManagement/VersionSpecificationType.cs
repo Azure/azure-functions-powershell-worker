52e874fa8668903bf360d8a0eb2a6700e3fcead9
Anatoli Beliaev <anatolib@microsoft.com>
2019-07-25 12:49:36 -0700

    Add a comment

--- src/DependencyManagement/VersionSpecificationType.cs
+++ src/DependencyManagement/VersionSpecificationType.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-#pragma warning disable 1591
+#pragma warning disable 1591 // Missing XML comment for publicly visible type or member 'member'
 
 namespace Microsoft.Azure.Functions.PowerShellWorker.DependencyManagement
 {
