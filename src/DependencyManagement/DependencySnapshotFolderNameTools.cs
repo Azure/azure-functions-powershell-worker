e37e85313494ee4f5b5957e00326c97fbc939494
Anatoli Beliaev <anatolib@microsoft.com>
2019-08-19 16:50:50 -0700

    Managed Dependencies: fixing cold start time regression (#311)
    * Change installed snapshot folder name pattern to *.r
    
    * Fix the doc

--- src/DependencyManagement/DependencySnapshotFolderNameTools.cs
+++ src/DependencyManagement/DependencySnapshotFolderNameTools.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.DependencyManagement
     {
         private const string InstallingPostfix = "i";
 
-        public const string InstalledPostfix = "r";
+        public const string InstalledPostfix = ".r";
 
         public const string InstalledPattern = "*" + InstalledPostfix;
 
@@ -29,10 +29,10 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.DependencyManagement
         /// appending a postfix, so that that the resulting path follows a different
         /// pattern and can be discovered using a different file mask.
         /// For example, for the _installed_ path
-        ///     ".../1907101234567r"
+        ///     ".../1907101234567.r"
         /// the _installing_ path will be:
-        ///     ".../1907101234567ri"
-        /// This makes it possible to enumerate all the installed snapshots by using ".../*r" mask,
+        ///     ".../1907101234567.ri"
+        /// This makes it possible to enumerate all the installed snapshots by using ".../*.r" mask,
         /// and all the installing snapshots by using ".../*i" mask.
         /// </summary>
         public static string ConvertInstalledToInstalling(string installedPath)
