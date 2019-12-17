e6a7745ea319c3009d86dc4cc905fc7c16fc9904
Anatoli Beliaev <anatolib@microsoft.com>
2019-12-17 15:52:10 -0800

    Expect HttpStatusCode.Accepted response

--- test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
+++ test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
@@ -1,29 +1,53 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.IO;
-using System.Net;
-using System.Threading.Tasks;
-using Xunit;
-
 namespace Azure.Functions.PowerShell.Tests.E2E
 {
+    using System.Net;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using Xunit.Abstractions;
+
     [Collection(Constants.FunctionAppCollectionName)]
     public class DurableEndToEndTests
     {
         private readonly FunctionAppFixture _fixture;
+        private readonly ITestOutputHelper _testOutputHelper;
 
-        public DurableEndToEndTests(FunctionAppFixture fixture)
+        public DurableEndToEndTests(FunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
         {
             this._fixture = fixture;
+            this._testOutputHelper = testOutputHelper;
         }
 
         [Fact]
-        public async Task ManagedDependenciesAreAvailable()
+        public async Task DurableClientFollowsAsyncPattern()
         {
-            var actualResponseMessage =
-                await Utilities.InvokeHttpTrigger("DurableClient", string.Empty, HttpStatusCode.Accepted);
+            var initialResponse = await Utilities.GetHttpTriggerResponse("DurableClient", queryString: string.Empty);
+            Assert.Equal(HttpStatusCode.Accepted, initialResponse.StatusCode);
+
+            var location = initialResponse.Headers.Location;
+            Assert.NotNull(location);
+
+            var initialResponseBody = await initialResponse.Content.ReadAsStringAsync();
+            dynamic initialResponseBodyObject = JsonConvert.DeserializeObject(initialResponseBody);
+            Assert.NotNull(initialResponseBodyObject.id);
+            var statusQueryGetUri = (string)initialResponseBodyObject.statusQueryGetUri;
+            Assert.Equal(location.ToString(), statusQueryGetUri);
+            Assert.NotNull(initialResponseBodyObject.sendEventPostUri);
+            Assert.NotNull(initialResponseBodyObject.purgeHistoryDeleteUri);
+            Assert.NotNull(initialResponseBodyObject.terminatePostUri);
+            Assert.NotNull(initialResponseBodyObject.rewindPostUri);
+
+            using (var httpClient = new HttpClient())
+            {
+                _testOutputHelper.WriteLine($"statusQueryGetUri: '{statusQueryGetUri}'");
+                var statusResponse = await httpClient.GetAsync(statusQueryGetUri);
+                Assert.Equal(HttpStatusCode.Accepted, statusResponse.StatusCode);
+            }
         }
     }
 }
