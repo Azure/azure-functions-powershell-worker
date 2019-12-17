33c2f321fc9143fb3265f4da12710ddeed7932ba
Anatoli Beliaev <anatolib@microsoft.com>
2019-12-17 13:16:22 -0800

    Add DurableEndToEndTests

---
+++ test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Azure.Functions.PowerShell.Tests.E2E
+{
+    [Collection(Constants.FunctionAppCollectionName)]
+    public class DurableEndToEndTests
+    {
+        private readonly FunctionAppFixture _fixture;
+
+        public DurableEndToEndTests(FunctionAppFixture fixture)
+        {
+            this._fixture = fixture;
+        }
+
+        [Fact]
+        public async Task ManagedDependenciesAreAvailable()
+        {
+            var actualResponseMessage =
+                await Utilities.InvokeHttpTrigger("DurableClient", string.Empty, HttpStatusCode.Accepted);
+        }
+    }
+}
