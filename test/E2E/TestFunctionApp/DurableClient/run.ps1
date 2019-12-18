9ccd36ca00c753a9a83ea14d1e2610f0b11ac54b
Anatoli Beliaev <anatolib@microsoft.com>
2019-12-17 17:27:04 -0800

    Fix orchestrator name

--- test/E2E/TestFunctionApp/DurableClient/run.ps1
+++ test/E2E/TestFunctionApp/DurableClient/run.ps1
@@ -4,7 +4,7 @@ param($Request, $TriggerMetadata)
 
 Write-Host "DurableClient started"
 
-$InstanceId = Start-NewOrchestration -FunctionName 'MyOrchestrator' -InputObject 'Hello'
+$InstanceId = Start-NewOrchestration -FunctionName 'DurableOrchestrator' -InputObject 'Hello'
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
 $Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
