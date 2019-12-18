9db4e28c085ffa592b3dfc221e43acf304b7c2c6
Anatoli Beliaev <anatolib@microsoft.com>
2019-12-17 21:21:22 -0800

    Set $ErrorActionPreference to 'Stop' in DurableOrchestrator; fix trace messages

--- test/E2E/TestFunctionApp/DurableOrchestrator/run.ps1
+++ test/E2E/TestFunctionApp/DurableOrchestrator/run.ps1
@@ -2,7 +2,9 @@ using namespace System.Net
 
 param($Context)
 
-Write-Host "MyOrchestrator: started. Input: $($Context.Input)"
+$ErrorActionPreference = 'Stop'
+
+Write-Host "DurableOrchestrator: started. Input: $($Context.Input)"
 
 $output = @()
 
@@ -10,6 +12,6 @@ $output += Invoke-ActivityFunction -FunctionName "SayHello" -Input "Tokyo"
 $output += Invoke-ActivityFunction -FunctionName "SayHello" -Input "Seattle"
 $output += Invoke-ActivityFunction -FunctionName "SayHello" -Input "London"
 
-Write-Host "MyOrchestrator: finished."
+Write-Host "DurableOrchestrator: finished."
 
 return $output
