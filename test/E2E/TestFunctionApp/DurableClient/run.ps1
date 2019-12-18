03bc98d9f4350f4822810484784251e11e8201ba
Anatoli Beliaev <anatolib@microsoft.com>
2019-12-17 17:26:08 -0800

    Fix trace messages

--- test/E2E/TestFunctionApp/DurableClient/run.ps1
+++ test/E2E/TestFunctionApp/DurableClient/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-Write-Host "HttpTrigger started"
+Write-Host "DurableClient started"
 
 $InstanceId = Start-NewOrchestration -FunctionName 'MyOrchestrator' -InputObject 'Hello'
 Write-Host "Started orchestration with ID = '$InstanceId'"
@@ -10,4 +10,4 @@ Write-Host "Started orchestration with ID = '$InstanceId'"
 $Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
 Push-OutputBinding -Name Response -Value $Response
 
-Write-Host "HttpTrigger completed"
+Write-Host "DurableClient completed"
