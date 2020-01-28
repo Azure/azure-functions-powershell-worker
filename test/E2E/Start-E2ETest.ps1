c9fe8da3906018bbcc32a947e07674fe21ded6c3
Anatoli Beliaev <anatolib@microsoft.com>
2020-01-27 23:07:51 -0800

    Fix worker.config.json deployment

--- test/E2E/Start-E2ETest.ps1
+++ test/E2E/Start-E2ETest.ps1
@@ -45,6 +45,7 @@ Write-Host "Copying azure-functions-powershell-worker to Functions Host workers 
 $configuration = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { 'Debug' }
 Remove-Item -Recurse -Force -Path "$FUNC_CLI_DIRECTORY/workers/powershell"
 Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/netcoreapp$NETCOREAPP_VERSION/publish/" "$FUNC_CLI_DIRECTORY/workers/powershell/$POWERSHELL_VERSION"
+Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/netcoreapp$NETCOREAPP_VERSION/publish/worker.config.json" "$FUNC_CLI_DIRECTORY/workers/powershell"
 
 Write-Host "Staring Functions Host..."
 
