a7f0db117e04d0ec0cd385c7c3232483b38d7318
Anatoli Beliaev <anatolib@microsoft.com>
2020-03-09 15:09:56 -0700

    Use int.MaxValue for gRPC message size limit (#424)
    Use int.MaxValue for gRPC message size limit instead of grpcMaxMessageLength command line argument

--- src/Worker.cs
+++ src/Worker.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.Functions.PowerShellWorker
                 .WithParsed(ops => arguments = ops)
                 .WithNotParsed(err => Environment.Exit(1));
 
-            var msgStream = new MessagingStream(arguments.Host, arguments.Port, arguments.MaxMessageLength);
+            var msgStream = new MessagingStream(arguments.Host, arguments.Port);
             var requestProcessor = new RequestProcessor(msgStream);
 
             // Send StartStream message
@@ -63,7 +63,7 @@ namespace Microsoft.Azure.Functions.PowerShellWorker
         [Option("requestId", Required = true, HelpText = "Request ID used for gRPC communication with the Host.")]
         public string RequestId { get; set; }
 
-        [Option("grpcMaxMessageLength", Required = true, HelpText = "gRPC Maximum message size.")]
+        [Option("grpcMaxMessageLength", Required = false, HelpText = "[Deprecated and ignored] gRPC Maximum message size.")]
         public int MaxMessageLength { get; set; }
     }
 }
