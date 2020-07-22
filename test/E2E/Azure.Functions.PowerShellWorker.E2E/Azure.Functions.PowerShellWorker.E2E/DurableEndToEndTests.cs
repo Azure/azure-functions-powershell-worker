90a4f2a809e68b921463fc1dc575ae5f48ea307f
Michael Peng <t-mipeng@microsoft.com>
2020-07-22 15:11:30 -0700

    Adjusted expected format for the CurrentUtcDateTime E2E test

--- test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
+++ test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
@@ -4,6 +4,7 @@
 namespace Azure.Functions.PowerShell.Tests.E2E
 {
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Threading.Tasks;
     using Xunit;
@@ -85,24 +86,46 @@ namespace Azure.Functions.PowerShell.Tests.E2E
             }
         }
 
+        /*
+            Verifies that the Durable execution model correctly replays the same collection of CurrentUtcDateTimes.
+            The orchestrator writes CurrentUtcDateTime values to a temporary file. File contents are expected to
+            take one of two forms:
+
+            Case 1                      Case 2
+            Line                        Line
+            0     ---                   0     ---
+            1     <Timestamp1>          1     <Timestamp1>    
+            2     <Timestamp1>          2     <Timestamp1>
+            3     ---                   3     ---
+            4     <Timestamp1>          4     <Timestamp1>
+            5     <Timestamp1>          5     <Timestamp1>
+            6     <Timestamp2>          6     <Timestamp2>
+            7     <Timestamp2>          7     <Timestamp2>
+            8     <Timestamp2>          8     <Timestamp2>
+            9     ---                   9     ---
+            10    <Timestamp1>          10    <Timestamp1>
+            11    <Timestamp1>          11    <Timestamp1>
+            12    <Timestamp2>          12    <Timestamp2>
+            13    <Timestamp2>          13    <Timestamp2>
+            14    <Timestamp2>          14    <Timestamp2>
+            15    <Timestamp3>          15    ---
+                                        16    <Timestamp1>
+                                        17    <Timestamp1>
+                                        18    <Timestamp2>
+                                        19    <Timestamp2>
+                                        20    <Timestamp2>
+                                        21    <Timestamp3>
+        */
         [Fact]
         public async Task DurableExecutionReplaysCurrentUtcDateTime()
         {
             var initialResponse = await Utilities.GetHttpTriggerResponse("CurrentUtcDateTimeStart", queryString: string.Empty);
-            Assert.Equal(HttpStatusCode.Accepted, initialResponse.StatusCode);
 
             var location = initialResponse.Headers.Location;
-            Assert.NotNull(location);
 
             var initialResponseBody = await initialResponse.Content.ReadAsStringAsync();
             dynamic initialResponseBodyObject = JsonConvert.DeserializeObject(initialResponseBody);
-            Assert.NotNull(initialResponseBodyObject.id);
             var statusQueryGetUri = (string)initialResponseBodyObject.statusQueryGetUri;
-            Assert.Equal(location.ToString(), statusQueryGetUri);
-            Assert.NotNull(initialResponseBodyObject.sendEventPostUri);
-            Assert.NotNull(initialResponseBodyObject.purgeHistoryDeleteUri);
-            Assert.NotNull(initialResponseBodyObject.terminatePostUri);
-            Assert.NotNull(initialResponseBodyObject.rewindPostUri);
 
             var orchestrationCompletionTimeout = TimeSpan.FromSeconds(60);
             var startTime = DateTime.UtcNow;
@@ -116,17 +139,6 @@ namespace Azure.Functions.PowerShell.Tests.E2E
                     {
                         case HttpStatusCode.Accepted:
                         {
-                            var statusResponseBody = await GetResponseBodyAsync(statusResponse);
-                            var runtimeStatus = (string)statusResponseBody.runtimeStatus;
-                            Assert.True(
-                                runtimeStatus == "Running" || runtimeStatus == "Pending",
-                                $"Unexpected runtime status: {runtimeStatus}");
-
-                            if (DateTime.UtcNow > startTime + orchestrationCompletionTimeout)
-                            {
-                                Assert.True(false, $"The orchestration has not completed after {orchestrationCompletionTimeout}");
-                            }
-
                             await Task.Delay(TimeSpan.FromSeconds(2));
                             break;
                         }
@@ -136,18 +148,28 @@ namespace Azure.Functions.PowerShell.Tests.E2E
                             Assert.Equal("Completed", (string)statusResponseBody.runtimeStatus);
                             string path = statusResponseBody.output.ToString();
                             string[] lines = System.IO.File.ReadAllLines(path);
-                            Assert.Equal("---", lines[0]);
-                            int[] delineatorIndices = { 0, 3, 9, 15 };
-                            int[] timestamp1Indices = { 1, 2, 4, 5, 10, 11, 16, 17 };
-                            int[] timestamp2Indices = { 6, 7, 8, 12, 13, 14, 18, 19, 20 };
+                            // Expect the format to be as in Case 1
+                            var delineatorLines = new List<int>(new int[] { 0, 3, 9 });
+                            var timestamp1Lines = new List<int>(new int[] { 1, 2, 4, 5, 10, 11 });
+                            var timestamp2Lines = new List<int>(new int[] { 6, 7, 8, 12, 13, 14 });
+                            int timestamp3Line = 15;
+                            
+                            // Updates the expected format to be Case 2 if it is not Case 1
+                            if (lines[timestamp3Line] == "---") {
+                                timestamp3Line = 21;
+                                delineatorLines.Add(15);
+                                timestamp1Lines.AddRange(new int[] { 16, 17 });
+                                timestamp2Lines.AddRange(new int[] { 18, 19, 20 });
+                            }
 
-                            // See the CurrentUtcDateTimeOrchestrator in the TestFunctionApp to see why these results are expected
-                            VerifyLinesEqual(lines: lines, equalIndices: delineatorIndices);
-                            VerifyLinesEqual(lines: lines, equalIndices: timestamp1Indices);
-                            VerifyLinesEqual(lines: lines, equalIndices: timestamp2Indices);
-                            Assert.NotEqual(lines[21], lines[0]);
-                            Assert.NotEqual(lines[21], lines[1]);
-                            Assert.NotEqual(lines[21], lines[6]);
+                            Assert.Equal("---", lines[delineatorLines[0]]);
+                            VerifyArrayItemsAreEqual(array: lines, indices: delineatorLines);
+                            VerifyArrayItemsAreEqual(array: lines, indices: timestamp1Lines);
+                            VerifyArrayItemsAreEqual(array: lines, indices: timestamp2Lines);
+                            // Verifies that the Timestamp3 line is not a delineator, Timestamp2, or Timestamp1 line
+                            Assert.NotEqual(lines[timestamp3Line], lines[delineatorLines[0]]);
+                            Assert.NotEqual(lines[timestamp3Line], lines[timestamp1Lines[0]]);
+                            Assert.NotEqual(lines[timestamp3Line], lines[timestamp2Lines[0]]);
                             return;
                         }
                         default:
@@ -161,14 +183,14 @@ namespace Azure.Functions.PowerShell.Tests.E2E
             
         }
         
-        private void VerifyLinesEqual(string[] lines, int[] equalIndices)
+        private void VerifyArrayItemsAreEqual(string[] array, List<int> indices)
         {
-            if (equalIndices.Length > 0)
+            if (indices.Capacity > 0)
             {
-                var expected = lines[equalIndices[0]];
-                for (int i = 1; i < equalIndices.Length; i++)
+                var expected = array[indices[0]];
+                for (int i = 1; i < indices.Capacity; i++)
                 {
-                    Assert.Equal(expected, lines[equalIndices[i]]);
+                    Assert.Equal(expected, array[indices[i]]);
                 }
             }
         }
