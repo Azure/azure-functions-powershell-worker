95a036694c70bd9b2f2f56197483dfd813c4153d
Michael Peng <t-mipeng@microsoft.com>
2020-08-07 13:12:58 -0700

    Added the Stop-DurableTimerTask cmdlet

---
+++ src/Durable/StopDurableTimerTaskCommand.cs
@@ -0,0 +1,32 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#pragma warning disable 1591 // Missing XML comment for publicly visible type or member 'member'
+
+namespace Microsoft.Azure.Functions.PowerShellWorker.Durable
+{
+    using System;
+    using System.Collections;
+    using System.Management.Automation;
+
+    /// <summary>
+    /// Start the Durable Functions timer
+    /// </summary>
+    [Cmdlet("Stop", "DurableTimerTask")]
+    public class StopDurableTimerCommand : PSCmdlet
+    {
+        /// <summary>
+        /// Gets and sets the duration of the Durable Timer.
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public DurableTimerTask TimerTask { get; set; }
+
+        protected override void EndProcessing()
+        {
+            TimerTask.Cancel();
+        }
+    }
+}
--- src/Modules/Microsoft.Azure.Functions.PowerShellWorker/Microsoft.Azure.Functions.PowerShellWorker.psd1
+++ src/Modules/Microsoft.Azure.Functions.PowerShellWorker/Microsoft.Azure.Functions.PowerShellWorker.psd1
@@ -54,12 +54,14 @@ FunctionsToExport = @(
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = @(
-    'Push-OutputBinding',
+    
     'Get-OutputBinding',
-    'Trace-PipelineObject',
+    'Invoke-ActivityFunction',
+    'Push-OutputBinding',
     'Set-FunctionInvocationContext',
     'Start-DurableTimer',
-    'Invoke-ActivityFunction',
+    'Stop-DurableTimerTask',
+    'Trace-PipelineObject',
     'Wait-ActivityFunction')
 
 # Variables to export from this module
