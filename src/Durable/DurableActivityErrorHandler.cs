635f3138f22521bc3d4b2c7b63d44427dd183423
Anatoli Beliaev <anatolib@microsoft.com>
2020-12-21 16:17:55 -0800

    Add a comment regarding ErrorId etc.

--- src/Durable/DurableActivityErrorHandler.cs
+++ src/Durable/DurableActivityErrorHandler.cs
@@ -17,6 +17,16 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Durable
 
         internal static void CreateAndWriteError(string errorMessage, Action<ErrorRecord> writeError)
         {
+            // This error is supposed to be documented and become a part of the public contract.
+            // When Function authors implement error handling in PowerShell, they may rely on
+            // the structure and the content of this error, specifically:
+            //   - ErrorId
+            //   - Exception type
+            //   - Exception message
+            //   - Error category
+            // If you make changes in any of these, make sure they are backward-compatible,
+            // so the existing Functions are not broken.
+
             const string ErrorId = "Functions.Durable.ActivityFailure";
             var exception = new ActivityFailureException(errorMessage);
             var errorRecord = new ErrorRecord(exception, ErrorId, ErrorCategory.NotSpecified, null);
