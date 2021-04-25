d8221ba0bcfe011f6d58c9e20dd8598b679deb44
Anatoli Beliaev <anatolib@microsoft.com>
2021-04-24 23:18:53 -0700

    Add Stop-DurableOrchestration example

--- examples/durable/DurableApp/InstanceManagement/run.ps1
+++ examples/durable/DurableApp/InstanceManagement/run.ps1
@@ -11,7 +11,16 @@ do {
     $Status = Get-DurableStatus -InstanceId $InstanceId -ShowHistory -ShowHistoryOutput -ShowInput
     Write-Host "Status: $($Status | ConvertTo-Json)"
     Start-Sleep -Seconds 5
-} while ($Status.runtimeStatus -eq 'Running')
+} while ($Status.runtimeStatus -ne 'Running')
+
+Write-Host "Terminating orchestration $InstanceId..."
+Stop-DurableOrchestration -InstanceId $InstanceId -Reason 'Terminated intentionally'
+
+do {
+    $Status = Get-DurableStatus -InstanceId $InstanceId -ShowHistory -ShowHistoryOutput -ShowInput
+    Write-Host "Status: $($Status | ConvertTo-Json)"
+    Start-Sleep -Seconds 5
+} while ($Status.runtimeStatus -ne 'Terminated')
 
 $Response = New-DurableOrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
 Push-OutputBinding -Name Response -Value $Response
