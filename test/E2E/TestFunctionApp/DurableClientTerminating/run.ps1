7fb926d379d8d91222265a1425e682bcf639e6ed
Anatoli Beliaev <anatolib@microsoft.com>
2021-04-25 00:38:49 -0700

    Add E2E test app

---
+++ test/E2E/TestFunctionApp/DurableClientTerminating/run.ps1
@@ -0,0 +1,19 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+Write-Host "DurableClient started"
+
+$ErrorActionPreference = 'Stop'
+
+$FunctionName = $Request.Query.FunctionName ?? 'DurableOrchestrator'
+
+$InstanceId = Start-DurableOrchestration -FunctionName $FunctionName -InputObject 'Hello'
+Write-Host "Started orchestration with ID = '$InstanceId'"
+
+Stop-DurableOrchestration -InstanceId $InstanceId -Reason 'Terminated intentionally'
+
+$Response = New-DurableOrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
+Push-OutputBinding -Name Response -Value $Response
+
+Write-Host "DurableClient completed"
