42c757412f28f3809e7559e654469490aa6f4556
Francisco-Gamino <frangom@microsoft.com>
2021-06-04 17:30:39 -0700

    Disable durable test for Stop-DurableOrchestration

--- test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
+++ test/E2E/Azure.Functions.PowerShellWorker.E2E/Azure.Functions.PowerShellWorker.E2E/DurableEndToEndTests.cs
@@ -415,7 +415,7 @@ namespace Azure.Functions.PowerShell.Tests.E2E
             }
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/Azure/azure-functions-powershell-worker/issues/640")]
         public async Task DurableClientTerminatesOrchestration()
         {
             var initialResponse = await Utilities.GetHttpTriggerResponse("DurableClientTerminating", queryString: string.Empty);
