61a551a7a3091cb92e71c1bb78b4b76b8caf3926
Francisco-Gamino <frangom@microsoft.com>
2021-08-03 23:20:19 -0700

    Upgrade TargetFramework to net5.0

--- test/E2E/Start-E2ETest.ps1
+++ test/E2E/Start-E2ETest.ps1
@@ -58,7 +58,7 @@ function NewTaskHubName
 $taskHubName = NewTaskHubName -Length 45
 
 $FUNC_RUNTIME_VERSION = '3'
-$NETCOREAPP_VERSION = '3.1'
+$TARGET_FRAMEWORK = 'net5.0'
 $POWERSHELL_VERSION = '7'
 
 $arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture.ToString().ToLowerInvariant()
@@ -111,8 +111,8 @@ if (-not $UseCoreToolsBuildFromIntegrationTests.IsPresent)
 
     $configuration = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { 'Debug' }
     Remove-Item -Recurse -Force -Path "$FUNC_CLI_DIRECTORY/workers/powershell"
-    Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/netcoreapp$NETCOREAPP_VERSION/publish/" "$FUNC_CLI_DIRECTORY/workers/powershell/$POWERSHELL_VERSION"
-    Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/netcoreapp$NETCOREAPP_VERSION/publish/worker.config.json" "$FUNC_CLI_DIRECTORY/workers/powershell"
+    Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/$TARGET_FRAMEWORK/publish/" "$FUNC_CLI_DIRECTORY/workers/powershell/$POWERSHELL_VERSION"
+    Copy-Item -Recurse -Force "$PSScriptRoot/../../src/bin/$configuration/$TARGET_FRAMEWORK/publish/worker.config.json" "$FUNC_CLI_DIRECTORY/workers/powershell"
 }
 
 Write-Host "Starting Functions Host..."
