005b02c5e2250874ba9b91d3130768f0d47d7d43
Francisco-Gamino <frangom@microsoft.com>
2021-09-13 18:45:22 -0700

    Update Start-E2ETest.ps1 to target Functions V4

--- test/E2E/Start-E2ETest.ps1
+++ test/E2E/Start-E2ETest.ps1
@@ -57,7 +57,7 @@ function NewTaskHubName
 
 $taskHubName = NewTaskHubName -Length 45
 
-$FUNC_RUNTIME_VERSION = '3'
+$FUNC_RUNTIME_VERSION = '4'
 $TARGET_FRAMEWORK = 'net6.0'
 $POWERSHELL_VERSION = '7.2'
 
