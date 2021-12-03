e9fa8e47a88358e2c85a9d0f49d8ea7ec2e0964f
Michael Peng <mpeng029@gmail.com>
2021-12-03 11:50:42 -0800

    Added RetryContext (#712)
    * Updated subtree from https://github.com/azure/azure-functions-language-worker-protobuf. Tag: v1.5.1-protobuf. Commit: 2fc14be59bbd8bb9f3f6275247047675b83fb3ca
    
    * Add RetryContext
    
    * Added tests
    
    * Added unit tests and removed E2E tests
    
    * Fix RetryContext unit test
    
    * Updated release_notes.md
    
    * Addressing requested changes

---
+++ src/Public/RetryContext.cs
@@ -0,0 +1,28 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+
+namespace Microsoft.Azure.Functions.PowerShellWorker
+{
+    /// <summary>
+    /// Custom RetryContext constructed from the RpcRetryContext member received from the host.
+    /// </summary>
+    internal class RetryContext
+    {
+        public RetryContext(int retryCount, int maxRetryCount, RpcException exception)
+        {
+            RetryCount = retryCount;
+            MaxRetryCount = maxRetryCount;
+            Exception = exception;
+        }
+
+        public int RetryCount { get; }
+
+        public int MaxRetryCount { get; }
+
+        public RpcException Exception { get; }
+    }
+}
