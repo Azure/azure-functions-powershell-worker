67c81ccf1c815cc67f74f6babf055c918fb3d4b2
Francisco Gamino <Francisco-Gamino@users.noreply.github.com>
2022-01-19 13:30:52 -0800

    Update PowerShell 7.2 to SDK 7.2.1  (#741)
    * Upgrade Microsoft.PowerShell.SDK to 7.2.1
    
    * Upgrade to .NET SDK 6.0.101
    
    * Upgrade to .NET SDK 3.1.416

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -12,13 +12,13 @@ $DotnetSDKVersionRequirements = @{
 
     # .NET SDK 3.1 is required by the Microsoft.ManifestTool.dll tool
     '3.1' = @{
-        MinimalPatch = '415'
-        DefaultPatch = '415'
+        MinimalPatch = '416'
+        DefaultPatch = '416'
     }
 
     '6.0' = @{
-        MinimalPatch = '100'
-        DefaultPatch = '100'
+        MinimalPatch = '101'
+        DefaultPatch = '101'
     }
 }
 
