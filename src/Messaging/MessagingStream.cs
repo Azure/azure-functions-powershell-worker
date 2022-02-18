c2ec3f72ab7f739030e9df7fe00ebdc1dd9b7c33
Michael Peng <michaelpeng@microsoft.com>
2022-02-18 15:01:06 -0800

    Add valid URL check and explicitly include credentials

--- src/Messaging/MessagingStream.cs
+++ src/Messaging/MessagingStream.cs
@@ -3,9 +3,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
 
@@ -13,23 +15,29 @@ namespace Microsoft.Azure.Functions.PowerShellWorker.Messaging
 {
     internal class MessagingStream
     {
-        private readonly Grpc.Core.AsyncDuplexStreamingCall<StreamingMessage, StreamingMessage> _call;
+        private readonly AsyncDuplexStreamingCall<StreamingMessage, StreamingMessage> _call;
         private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(initialCount: 1, maxCount: 1);
 
         internal MessagingStream(string host, int port)
         {
             // To call unsecured gRPC services, ensure the address starts with 'http' as opposed to 'https'.
             // For more detail, see https://docs.microsoft.com/en-us/aspnet/core/grpc/client?view=aspnetcore-6.0
-            string address = $"http://{host}:{port}";
+            string uriString = $"http://{host}:{port}";
+            if (!Uri.TryCreate(uriString, UriKind.Absolute, out Uri grpcUri))
+            {
+                throw new InvalidOperationException($"The gRPC channel URI '{uriString}' could not be parsed.");
+            }
+
             const int maxMessageLength = int.MaxValue;
 
             var channelOptions = new GrpcChannelOptions
             {
                 MaxReceiveMessageSize = maxMessageLength,
-                MaxSendMessageSize = maxMessageLength
+                MaxSendMessageSize = maxMessageLength,
+                Credentials = ChannelCredentials.Insecure
             };
 
-            GrpcChannel channel = GrpcChannel.ForAddress(address, channelOptions);
+            GrpcChannel channel = GrpcChannel.ForAddress(grpcUri, channelOptions);
             _call = new FunctionRpc.FunctionRpcClient(channel).EventStream();
         }
 
