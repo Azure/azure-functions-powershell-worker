6e8c424cd6f2c45ed6239e2466bafc6019389060
Francisco-Gamino <frangom@microsoft.com>
2022-05-17 16:04:12 -0700

    Upgrade to .NET SDK 3.1.419

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -11,8 +11,8 @@ $RepoRoot = (Resolve-Path "$PSScriptRoot/..").Path
 $DotnetSDKVersionRequirements = @{
     # We need .NET SDK 3.1 for running the tests, as we still build against the 3.1 framework
     '3.1' = @{
-        MinimalPatch = '417'
-        DefaultPatch = '417'
+        MinimalPatch = '419'
+        DefaultPatch = '419'
     }
     # We need .NET SDK 5.0 for the updated C# compiler
     '5.0' = @{
