5becc6168705175a794277314e007cb88b601b80
Francisco-Gamino <frangom@microsoft.com>
2022-05-18 17:30:56 -0700

    Remove downloading 'Utility.psd1' and 'Management.psd1' from build

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -164,19 +164,6 @@ function Resolve-ProtoBufToolPath
     }
 }
 
-function Get-WebFile {
-    param (
-        [string] $Url,
-        [string] $OutFile
-    )
-    $directoryName = [System.IO.Path]::GetDirectoryName($OutFile)
-    if (!(Test-Path $directoryName)) {
-        New-Item -Type Directory $directoryName
-    }
-    Remove-Item $OutFile -ErrorAction SilentlyContinue
-    Invoke-RestMethod $Url -OutFile $OutFile
-}
-
 function Write-Log
 {
     param(
