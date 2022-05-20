1fb95c2a40c90e576aac4a29ffdc2ed00bf5dc2b
Francisco Gamino <Francisco-Gamino@users.noreply.github.com>
2022-05-20 12:03:50 -0700

    Update PowerShell 7.0 to SDK 7.0.11 (#809) (#810)
    * Upgrade to .NET SDK 3.1.419
    
    * Upgrade to .NET SDK 5.0.408
    
    * Upgrade Microsoft.PowerShell.SDK to 7.0.11

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -11,13 +11,13 @@ $RepoRoot = (Resolve-Path "$PSScriptRoot/..").Path
 $DotnetSDKVersionRequirements = @{
     # We need .NET SDK 3.1 for running the tests, as we still build against the 3.1 framework
     '3.1' = @{
-        MinimalPatch = '417'
-        DefaultPatch = '417'
+        MinimalPatch = '419'
+        DefaultPatch = '419'
     }
     # We need .NET SDK 5.0 for the updated C# compiler
     '5.0' = @{
-        MinimalPatch = '406'
-        DefaultPatch = '406'
+        MinimalPatch = '408'
+        DefaultPatch = '408'
     }
 }
 
