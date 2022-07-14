ec33bbbcf1b73abee9406d868d2ccf2a3eeb4a20
Anthony Staples <t-anstaples@microsoft.com>
2022-07-14 11:01:05 -0700

    Change to use module indexing

---
+++ src/WorkerIndexing/FunctionInformation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsHelpers
+{
+    internal class FunctionInformation
+    {
+        public string Directory { get; set; } = "";
+        public string ScriptFile { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string EntryPoint { get; set; } = "";
+        public string FunctionId { get; set; } = "";
+        public List<BindingInformation> Bindings { get; set; } = new List<BindingInformation>();
+
+        internal RpcFunctionMetadata ConvertToRpc()
+        {
+            RpcFunctionMetadata returnMetadata = new RpcFunctionMetadata();
+            returnMetadata.FunctionId = FunctionId;
+            returnMetadata.Directory = Directory;
+            returnMetadata.EntryPoint = EntryPoint;
+            returnMetadata.Name = Name;
+            returnMetadata.ScriptFile = ScriptFile;
+            foreach(BindingInformation binding in Bindings)
+            {
+                string rawBinding = binding.ConvertToRpcRawBinding(out BindingInfo bindingInfo);
+                returnMetadata.Bindings.Add(binding.Name, bindingInfo);
+                returnMetadata.RawBindings.Add(rawBinding);
+            }
+            return returnMetadata;
+        }
+    }
+}
---
+++ src/WorkerIndexing/WorkerIndexingHelper.cs
@@ -0,0 +1,53 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using AzureFunctionsHelpers;
+using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text.Json;
+
+namespace Microsoft.Azure.Functions.PowerShellWorker.WorkerIndexing
+{
+    internal class WorkerIndexingHelper
+    {
+        internal static IEnumerable<RpcFunctionMetadata> IndexFunctions(string baseDir)
+        {
+            List<RpcFunctionMetadata> indexedFunctions = new List<RpcFunctionMetadata>();
+
+            InitialSessionState initial = InitialSessionState.CreateDefault();
+            initial.ImportPSModule(new string[] { "AzureFunctionsHelpers" });
+            Runspace runspace = RunspaceFactory.CreateRunspace(initial);
+            runspace.Open();
+            System.Management.Automation.PowerShell ps = System.Management.Automation.PowerShell.Create();
+            ps.Runspace = runspace;
+            //ps.AddCommand("Import-Module").AddArgument("C:\\Program Files\\WindowsPowerShell\\Modules\\AzureFunctionsHelpers\\AzureFunctionsHelpers.dll").Invoke();
+            ps.AddCommand("Get-AzureFunctionsMetadata").AddArgument("C:\\Users\\t-anstaples\\source\\powershell\\apat2");
+            string outputString = string.Empty;
+            foreach (PSObject rawMetadata in ps.Invoke())
+            {
+                if (outputString != string.Empty)
+                {
+                    throw new Exception("Multiple results from metadata cmdlet");
+                }
+                outputString = rawMetadata.ToString();
+                Console.WriteLine(rawMetadata.ToString());
+            }
+            ps.Commands.Clear();
+
+            List<FunctionInformation> functionInformations = JsonConvert.DeserializeObject<List<FunctionInformation>>(outputString);
+
+            foreach(FunctionInformation fi in functionInformations)
+            {
+                indexedFunctions.Add(fi.ConvertToRpc());
+            }
+
+            return indexedFunctions;
+        }
+    }
+}
