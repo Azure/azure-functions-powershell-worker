c8bf1b9ad86eac796af188079d854397b403398e
Francisco-Gamino <frangom@microsoft.com>
2022-11-09 19:22:18 -0800

    Upgrade Grpc.Tools to 2.50.0

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -22,7 +22,7 @@ $DotnetSDKVersionRequirements = @{
     }
 }
 
-$GrpcToolsVersion = '2.43.0' # grpc.tools
+$GrpcToolsVersion = '2.50.0' # grpc.tools
 $GoogleProtobufToolsVersion = '3.19.4' # google.protobuf.tools
 
 function Find-Dotnet
