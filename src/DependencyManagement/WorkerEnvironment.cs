9705debeff904c50356cae3ff9010441885b7620
Francisco Gamino <Francisco-Gamino@users.noreply.github.com>
2023-04-28 14:02:15 -0700

    Managed Dependencies is not supported on Legion (#945) (#946)
    * Error out if the user tries to install modules via Managed Dependencies on Legion
    
    * Moved logic to help figure out if we are running in Azure to a helper class
    
    * Add test case to validate that we do not throw on Legion when requirements.psd1 has no entries
    
    * Add Managed Dependencies is not supported on Legion test case

---
+++ src/DependencyManagement/WorkerEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Functions.PowerShellWorker.DependencyManagement
+{
+    /// <summary>
+    /// Hold information about the environment.
+    /// </summary>
+    internal static class WorkerEnvironment
+    {
+        // Environment variables to help figure out if we are running in Azure.
+        private const string AzureWebsiteInstanceId = "WEBSITE_INSTANCE_ID";
+        private const string ContainerName = "CONTAINER_NAME";
+        private const string LegionServiceHost = "LEGION_SERVICE_HOST";
+
+        public static bool IsAppService()
+        {
+            return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(AzureWebsiteInstanceId));
+        }
+
+        public static bool IsLinuxConsumption()
+        {
+            return !IsAppService() && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(ContainerName));
+        }
+
+        public static bool IsLinuxConsumptionOnLegion()
+        {
+            return !IsAppService() &&
+                   !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(ContainerName)) &&
+                   !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(LegionServiceHost));
+        }
+    }
+}
