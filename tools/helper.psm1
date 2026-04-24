cb9405ca99fa01cc8d0bbadee1426b89fc0d58ed
Andy Staples <andystaples@microsoft.com>
2026-04-24 10:17:24 -0600

    Also update PS SDK to 7.6.1

--- tools/helper.psm1
+++ tools/helper.psm1
@@ -17,8 +17,8 @@ $DotnetSDKVersionRequirements = @{
     }
 
     '10.0' = @{
-        MinimalPatch = '101'
-        DefaultPatch = '101'
+        MinimalPatch = '202'
+        DefaultPatch = '202'
     }
 }
 
